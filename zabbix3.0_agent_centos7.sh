77767168897950c72d123552079ecb54e98c542c
ThorSiv <thorsivodin@gmail.com>
2017-04-17 20:47:49 +0800

    add shell cript

--- zabbix3.0_agent_centos7.sh
+++ zabbix3.0_agent_centos7.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if test $# -ne 1;then
+ROLE=`whoami`
+if test $# -ne 2;then
 	echo 
 	echo
 	echo "	This script for installing saltminion on ubuntu14.04"
@@ -8,17 +9,39 @@ if test $# -ne 1;then
 	echo
 	exit
 fi
+if test $ROLE != "root";then
+	echo 
+	echo
+	echo "This script should be run in root."
+	echo "exit"
+	echo
+	exit
+fi
 
+echo 'install zabbix yum repo....'
 yum -y install http://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm &>/dev/null
+
+echo 'clean yum cache....'
 yum clean all  &>/dev/null
 yum makecache  &>/dev/null
+
+echo 'install zabbix-agent....'
 yum install zabbix-agent -y  &>/dev/null
 
 if test $? -eq 0;then 
-	mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.default
+	mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.default &>/dev/null
 	sed "s/Server=127.0.0.1/Server=$1/" /etc/zabbix/zabbix_agentd.default > /etc/zabbix/zabbix_agentd.conf
-	sed -i "s/Hostname=Zabbix server/Hostname=$2/" /etc/zabbix/zabbix_agentd.conf
-	service zabbix-agent start
+	sed -i "s/Hostname=.*/Hostname=$2/" /etc/zabbix/zabbix_agentd.conf
+
+	echo 'start zabbix-agent...'
+	service zabbix-agent start &>/dev/null
+
+	if test $? -eq 0;then
+		echo 'Success started zabbix-agent!!!'
+	else
+		echo 'Failed to start zabbix-agent :('
+		exit
+	fi
 else
 	echo
 	echo
